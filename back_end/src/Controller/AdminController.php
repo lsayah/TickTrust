0660df870a11635e0d7bf09d949ba7255bda062c
Fogg303 <kpataclom56@gmail.com>
2025-02-03 10:37:43 +0100

    gestion_de_profil

--- back_end/src/Controller/AdminController.php
+++ back_end/src/Controller/AdminController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserRoleType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -11,10 +15,61 @@ class AdminController extends AbstractController
 {
     /**
      * @Route("/admin/dashboard", name="admin_dashboard")
-     * @IsGranted("admin")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function dashboard(): Response
     {
         return $this->render('admin/dashboard_admin.html.twig');
+    }
+/**
+ * @Route("/admin/users", name="admin_user_list")
+ */
+// @IsGranted("ROLE_ADMIN")
+public function listUsers(EntityManagerInterface $entityManager): Response
+{
+    $users = $entityManager->getRepository(User::class)->findAll();
+
+    return $this->render('admin/user/index.html.twig', [
+        'users' => $users,
+    ]);
+}
+
+
+    /**
+     * @Route("/admin/users/edit/{id}", name="admin_user_edit")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function editUser(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserRoleType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Rôle de l\'utilisateur mis à jour avec succès.');
+            return $this->redirectToRoute('admin_user_list');
+        }
+
+        return $this->render('admin/user/edit.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/users/delete/{id}", name="admin_user_delete", methods={"POST"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function deleteUser(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Utilisateur supprimé avec succès.');
+        }
+
+        return $this->redirectToRoute('admin_user_list');
     }
 }
---
+++ back_end/src/Form/ChangePasswordType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Nouveau mot de passe',
+                    'attr' => ['class' => 'form-control'],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le nouveau mot de passe',
+                    'attr' => ['class' => 'form-control'],
+                ],
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Changer le mot de passe',
+                'attr' => ['class' => 'btn btn-success mt-3'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
